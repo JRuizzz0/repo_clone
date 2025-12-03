ef197d08ac23faed4ff578241e999be581c9dd71
Rubinzo <random.randint.nft@gmail.com>
2025-12-03 11:11:21 +0100

    Mejora de gastar dinero

--- Personaje.java
+++ Personaje.java
@@ -1,31 +1,37 @@
 import java.util.Scanner;
 
 public class Personaje {
- private String nom;
- private static int dinero;
- private static final Scanner sc = new Scanner(System.in);
- public Personaje(String nom, int dinero) {
- this.nom = nom;
- this.dinero = dinero;
- }
- public void mostrarInfo() {
- System.out.println(nom + " tiene " + dinero + "$.");
- }
- public static void main(String[] args) {
- Personaje jugador = new Personaje("Trevor", 500);
- jugador.mostrarInfo();
- jugador.gastarDinero();
-}
-public static int gastarDinero(){
-    
-    System.out.println("Cuanto dinero te quieres gastar en ropa: ");
-    int cantidad = sc.nextInt();
-    int dinerototal = dinero - cantidad;
-    System.out.println("Trevor se ha gastado " + cantidad + ", le quedan " + dinerototal);
+    private String nom;
+    private static int dinero;
+    private static final Scanner sc = new Scanner(System.in);
+    public Personaje(String nom, int dinero) {
+        this.nom = nom;
+        this.dinero = dinero;
+    }
+    public void mostrarInfo() {
+        System.out.println(nom + " tiene " + dinero + "$.");
+    }
+    public static void main(String[] args) {
+        Personaje jugador = new Personaje("Trevor", 500);
+        jugador.mostrarInfo();
+        jugador.gastarDinero(jugador);
+    }
+    public void gastarDinero(Personaje jugador) {
+
+        System.out.println("Cuanto dinero te quieres gastar en ropa: ");
+        int cantidad = sc.nextInt();
+        if (cantidad < jugador.dinero) {
+            int dinerototal = dinero - cantidad;
+            System.out.println("Trevor se ha gastado " + cantidad + "$, le quedan " + dinerototal + "$");
+        }else  {
+            System.out.println("No puedes gastarte más de "+ dinero + "$");
+        }
 
 
-return dinerototal;
-}
 
- 
+
+
+    }
+
+
 }
