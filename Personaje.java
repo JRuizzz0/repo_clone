5ab3fa87bf3878f724b5a0eeb428062fbbcdd8ca
JRuizzz0 <javiru01ja@gmail.com>
2025-12-03 11:05:57 +0100

    Segundo commit

--- Personaje.java
+++ Personaje.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class Personaje {
  private String nom;
- private int dinero;
+ private static int dinero;
+ private static final Scanner sc = new Scanner(System.in);
  public Personaje(String nom, int dinero) {
  this.nom = nom;
  this.dinero = dinero;
@@ -11,6 +14,18 @@ public class Personaje {
  public static void main(String[] args) {
  Personaje jugador = new Personaje("Trevor", 500);
  jugador.mostrarInfo();
- }
+ jugador.gastarDinero();
+}
+public static int gastarDinero(){
+    
+    System.out.println("Cuanto dinero te quieres gastar en ropa: ");
+    int cantidad = sc.nextInt();
+    int dinerototal = dinero - cantidad;
+    System.out.println("Trevor se ha gastado " + cantidad + ", le quedan " + dinerototal);
+
+
+return dinerototal;
 }
 
+ 
+}
